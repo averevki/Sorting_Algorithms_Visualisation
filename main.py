1fd8b93187e5f52180f6bfaab4628c3a9cc3a2e5
Aleksandr Verevkin <sandyverevkin@gmail.com>
2022-03-26 15:59:14 +0100

    feat: update project status

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 """Sorting Algorithms Visualization using tkinter
+
 Program provided with python unit tests
 """
 __author__ = "Aleksandr Verevkin"
-__license__ = "GPL 3.0"
-__status__ = "production"
+__license__ = "GNU GPL v3"
+__status__ = "maintenance"
 __maintainer__ = "Aleksandr Verevkin"
+
 from screen import Screen
-# TODO add reverse orders
-# TODO update UI
-# TODO add Bitonic Sort, Pigeonhole Sort
-# TODO markdown documentation
 
 
 def main():
